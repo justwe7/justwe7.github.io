8406f9af5f8ca1b875a49442687b1d14cd3c5ad5
lihx <ilihuaxi@gmail.com>
2021-03-02 20:28:19 +0800

    test: cp

--- copy.sh
+++ copy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "登录..."
-export SSHPASS=$DEP_PASS
+# export SSHPASS=$DEP_PASS
+touch pw.txt
+echo "$DEP_PASS" > pw.txt
 # sshpass -e ssh -o stricthostkeychecking=no tudou@120.53.221.250
 ls
 echo "复制..."
 mkdir nihao
-sshpass -p ssh rsync -avzu tudou@120.53.221.250:/www/wwwroot/chevereto/images nihao
+sshpass -f pw.txt scp tudou@120.53.221.250:/www/wwwroot/chevereto/images nihao
 echo "复制完成..."
 ls
 cd nihao
