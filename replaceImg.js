513292f0881761e5e2a3c3c2ee8a6c849ff3f232
lihx <ilihuaxi@gmail.com>
2021-03-02 19:24:40 +0800

    feat: replaceImg

--- replaceImg.js
+++ replaceImg.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 const list = globby.sync(['**/*.md', '!*.md'], {gitignore: true})
 
 //读取文件，并且替换文件中指定的字符串
-const replaceFile = function(filePath,sourceRegx,targetStr) {
+const replaceFile = function(filePath, sourceRegx, targetStr) {
   fs.readFile(filePath,function(err,data){
     if(err){
         return err;
     }
     let str = data.toString();
-    str = str.replace(sourceRegx,targetStr);
+    const reg = new RegExp(sourceRegx, 'g')
+    str = str.replace(reg,targetStr);
     fs.writeFile(filePath, str, function (err) {
         if (err) return err;
   });
